6bd82c48e1e0b1b59ae41e63b8958c1649d7b0af
SSDesth <carlosmontt1993@gmail.com>
2019-09-26 22:22:53 -0600

    Arbol funcional
    el arbol ya esta funcionando

---
+++ Info Empleo/arbol.js
@@ -0,0 +1,208 @@
+
+
+var arbolDatos = [
+  {
+    "name": "Costa Rica",
+     "cant":"54184",
+    "parent": "null",
+    "children": [
+      {
+        "name": "Level 2: A",
+        "parent": "Top Level",
+        "cant":"20000",
+        "children": [
+          {
+            "name": "Son of A",
+            "cant":"10000",
+            "parent": "Level 2: A"
+          },          {
+            "name": "Son of A",
+            "cant":"7000",
+            "parent": "Level 2: A"
+          },
+          {
+            "name": "Daughter of A",
+            "cant":"3000",
+            "parent": "Level 2: A"
+          }
+        ]
+      },
+      {
+        "name": "Level 2: B",
+        "cant":"10000",
+        "parent": "Top Level"
+      },      {
+        "name": "Level 2: B",
+        "cant":"24184",
+        "parent": "Top Level"
+      }
+    ]
+  }
+];
+
+
+//margen de la pantalla
+var margin = {top: 20, right: 120, bottom: 20, left: 120},
+	width = 900 - margin.right - margin.left,
+	height = 500 - margin.top - margin.bottom;
+
+//tiempo duracion nodos al desplegarse o clickear
+var i = 0,
+	duration = 800,
+	root;
+//dimensiones arbol
+var arbol = d3.layout.tree()
+	.size([height, width]);
+
+var diagonal = d3.svg.diagonal()
+	.projection(function(d) { return [d.y, d.x]; });
+
+var svg = d3.select("body").append("svg")
+	.attr("width", width + margin.right + margin.left)
+	.attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .data(arbolDatos)
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+root = arbolDatos[0];
+root.x0 = height / 2;
+root.y0 = 0;
+ // carga el arbol con los datos
+update(root);
+//Posicion del primer nodo
+d3.select(self.frameElement).style("height", "500px");
+
+function update(source) {
+
+  var nodes = arbol.nodes(root).reverse(),
+	  links = arbol.links(nodes);
+
+ 
+  nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+
+  var node = svg.selectAll("g.node")
+	  .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+
+  var nodeEnter = node.enter().append("g")
+	  .attr("class", "node")
+	  .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+	  .on("click", click)
+    .on("mouseover", function(d,i){
+      var coordinates= d3.mouse(this);
+      var x = d3.event.pageX + 15;
+      var y = d3.event.pageY+ 15;
+
+
+      document.getElementById("mostrarInfo").style.top= y + "px";
+      document.getElementById("mostrarInfo").style.left= x + "px";
+
+      document.getElementById("infoNombre").innerHTML = d.name;
+      document.getElementById("infoCant").innerHTML = d.cant;
+    })
+    .on("mouseout", function(d,i){
+      document.getElementById("mostrarInfo").style.top="-500px";
+      document.getElementById("mostrarInfo").style.left="-500px";
+
+    })
+    ;
+
+  nodeEnter.append("circle")
+	  .attr("r", 100)
+	  .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+  nodeEnter.append("text")
+	  .attr("x", function(d) { return d.children || d._children ? -45: 30; })
+	  .attr("dy", ".35em")
+	  .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+	  .text(function(d) { return d.name; })
+	  .style("fill-opacity", 1e-6);
+
+  
+  var nodeUpdate = node.transition()
+	  .duration(duration)
+	  .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+  nodeUpdate.select("circle")
+	  .attr("r", function(d) { return ((d.cant*100)/55000)/2.5})
+	  .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+  nodeUpdate.select("text")
+	  .style("fill-opacity", 1);
+
+
+  var nodeExit = node.exit().transition()
+	  .duration(duration)
+	  .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+	  .remove();
+
+  nodeExit.select("circle")
+	  .attr("r", 1e-6);
+
+  nodeExit.select("text")
+	  .style("fill-opacity", 1e-6);
+
+  
+  var link = svg.selectAll("path.link")
+	  .data(links, function(d) { return d.target.id; });
+
+
+  link.enter().insert("path", "g")
+	  .attr("class", "link")
+	  .attr("d", function(d) {
+		var o = {x: source.x0, y: source.y0};
+		return diagonal({source: o, target: o});
+	  });
+
+
+  link.transition()
+	  .duration(duration)
+	  .attr("d", diagonal);
+
+
+  link.exit().transition()
+	  .duration(duration)
+	  .attr("d", function(d) {
+		var o = {x: source.x, y: source.y};
+		return diagonal({source: o, target: o});
+	  })
+	  .remove();
+
+  
+  nodes.forEach(function(d) {
+	d.x0 = d.x;
+	d.y0 = d.y;
+  });
+}
+
+
+var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function (d) {
+        return "<strong>Cant\u00F3n:</strong> <span style='color:red'>" + d.nombre + "</span> <br>" +
+            "<strong>Poblaci\u00F3n:</strong> <span style='color:red'>" + d.poblacion + " habitantes</span> <br>" +
+            "<strong>\u00C1rea:</strong> <span style='color:red'>" + d.area + " km²</span> <br>" +
+            "<strong>Densidad:</strong> <span style='color:red'>" + d.densidad + " habitantes/km²</span> <br>";
+    });
+
+//Aplico los tooltips
+svg.call(tip);
+
+function click(d) {
+  
+ /* document.getElementById("infoNombre").innerHTML = d.name;
+  document.getElementById("infoCant").innerHTML = d.cant;
+*/
+  if (d.children) {
+	d._children = d.children;
+	d.children = null;
+  } else {
+	d.children = d._children;
+	d._children = null;
+  }
+  
+  update(d);
+
+}
